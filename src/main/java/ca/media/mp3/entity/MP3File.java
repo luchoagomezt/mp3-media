cad4f7a11b18b7b0842be638922b526efdf1031a
Luis A Gomez <luchoagomezt@gmail.com>
2018-06-23 00:41:31 -0400

    Adding test cases for empty and null MP3 array.

--- src/main/java/ca/media/mp3/entity/MP3File.java
+++ src/main/java/ca/media/mp3/entity/MP3File.java
@@ -1,11 +1,25 @@
 package ca.media.mp3.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MP3File {
   private List<ID3V2Tag> tags;
   private List<MP3Frame> frames;
     
-  public MP3File(byte[] mp3file) {    
+  public MP3File(byte[] mp3Array) throws IllegalArgumentException {
+    if (mp3Array == null) {
+      throw new IllegalArgumentException("Byte array is null"); 
+    }
+    
+    tags = new ArrayList<ID3V2Tag>();
+    frames = new ArrayList<MP3Frame>();
+    if (mp3Array.length == 0) {
+      return;
+    }
+  }
+  
+  public List<ID3V2Tag> getID3V2Tags() {
+    return tags;
   }
 }
--- src/test/java/ca/media/mp3/entity/MP3FileTest.java
+++ src/test/java/ca/media/mp3/entity/MP3FileTest.java
@@ -1,5 +1,18 @@
 package ca.media.mp3.entity;
 
-public class MP3FileTest {
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertTrue;
 
+@Test
+public class MP3FileTest {
+  @Test
+  public void mp3ArrayIsEmpty() {
+    MP3File file = new MP3File(new byte[]{});
+    assertTrue(file.getID3V2Tags().isEmpty());
+  }
+  
+  @Test(expectedExceptions = {IllegalArgumentException.class})
+  public void mp3ArrayIsNull() {
+    new MP3File(null);
+  }
 }
