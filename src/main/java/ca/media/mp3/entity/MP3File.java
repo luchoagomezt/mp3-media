30753451281728513c1083c5c604d9fea61ded28
Luis A Gomez <luchoagomezt@gmail.com>
2018-06-23 13:29:45 -0400

    Refactoring.

--- src/main/java/ca/media/mp3/entity/MP3File.java
+++ src/main/java/ca/media/mp3/entity/MP3File.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class MP3File {
   private List<ID3V2Tag> tags;
-  private List<MP3Frame> frames;
     
   public MP3File(byte[] mp3Array) throws IllegalArgumentException {
     if (mp3Array == null) {
@@ -13,7 +12,6 @@ public class MP3File {
     }
     
     tags = new ArrayList<ID3V2Tag>();
-    frames = new ArrayList<MP3Frame>();
     if (mp3Array.length == 0) {
       return;
     }
--- src/main/java/ca/media/mp3/entity/MP3Frame.java
+++
@@ -1,7 +0,0 @@
-package ca.media.mp3.entity;
-
-public class MP3Frame {
-  
-  public MP3Frame(byte[] frame) {
-  }
-}
