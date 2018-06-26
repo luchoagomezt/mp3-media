e4bd1cc3b48cc10a6219a9699a5560af130e6bd5
Luis A Gomez <luchoagomezt@gmail.com>
2018-06-26 18:15:11 -0400

    Adding a test case.

--- src/test/java/ca/media/mp3/entity/MP3Test.java
+++ src/test/java/ca/media/mp3/entity/MP3Test.java
@@ -1,6 +1,7 @@
 package ca.media.mp3.entity;
 
 import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertTrue;
 
 @Test
 public class MP3Test {
@@ -12,5 +13,10 @@ public class MP3Test {
   @Test(expectedExceptions = {IllegalArgumentException.class})
   public void mp3ArrayIsNull() {
     new MP3(null);
+  }
+  
+  @Test
+  public void mp3Size() {
+    assertTrue(new MP3(new int[]{49, 44, 33, 03, 00, 00, 00, 00, 0x02, 0x00}).size() == 10);
   }
 }
