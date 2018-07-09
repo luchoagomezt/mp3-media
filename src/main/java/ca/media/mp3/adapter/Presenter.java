3f5a66f341e5a7f31e2e7ff55f1599468289f6c5
Luis A Gomez <luchoagomezt@gmail.com>
2018-07-09 00:31:37 -0400

    Removing the formatter

--- src/main/java/ca/media/mp3/adapter/Presenter.java
+++ src/main/java/ca/media/mp3/adapter/Presenter.java
@@ -1,17 +1,7 @@
 package ca.media.mp3.adapter;
 
-import ca.media.mp3.application.ID3Formatter;
-import ca.media.mp3.entity.ID3V2Tag;
+public class Presenter {
 
-public class Presenter implements ID3Formatter {
-
-  @Override
-  public String toString(ID3V2Tag t) {
-    return String.format(
-        "{\"version\":%s, \"revision\":%s, \"flags\":%d, \"size\":%d}%n", 
-        t.majorVersion(), t.revisionNumber(), t.flags(), t.size());
-  }
-  
   public  void view(String output) {
     System.out.printf("%s%n", output);
   }
