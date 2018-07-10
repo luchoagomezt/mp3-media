ce47c681ee80bbe19c7d71aed1c0367725b95fee
LUIS ALFONSO GOMEZ <LUIS.GOMEZ@cbc.ca>
2018-07-10 16:24:12 -0400

    Adding a Map to String method.

--- src/main/java/ca/media/mp3/adapter/Presenter.java
+++ src/main/java/ca/media/mp3/adapter/Presenter.java
@@ -1,8 +1,19 @@
 package ca.media.mp3.adapter;
 
+import java.util.Map;
+
 public class Presenter {
 
+  public String mapToString(Map<String, Integer> map) {
+    return map.
+        entrySet().
+        stream().
+        map(e -> "\"" + e.getKey() + "\":" + e.getValue()).
+        reduce((s1, s2) -> s1 + ", " + s2).
+        orElse("");
+  }
+ 
   public  void view(String output) {
-    System.out.printf("%s%n", output);
+    System.out.printf("{%s}%n", output);
   }
 }
