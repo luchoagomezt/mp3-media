89c889e1b815187fb3ffd61b6bfcba9c5d835637
Luis A Gomez <luchoagomezt@gmail.com>
2018-07-11 01:03:24 -0400

    Removing redundant test case

--- src/test/java/ca/media/mp3/adapter/PresenterTest.java
+++ src/test/java/ca/media/mp3/adapter/PresenterTest.java
@@ -8,11 +8,6 @@ import static org.testng.Assert.assertNull;
 public class PresenterTest {
 
   @Test
-  public void viewWithNullParameter() {
-    new Presenter().view(null);
-  }
-
-  @Test
   public void tagToStringWithNullParameter() {
     assertNull(new Presenter().tagToString(null));
   }
@@ -20,6 +15,6 @@ public class PresenterTest {
   @Test
   public void tagToStringWithNonNullParameter() {
     assertEquals(new Presenter().tagToString(new ID3V2Tag(new int[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x02, 0x00})), 
-    "\"size\":256, \"revisionNumber\":0, \"flags\":0, \"majorVersion\":3");
+    "{\"size\":256, \"revisionNumber\":0, \"flags\":0, \"majorVersion\":3}");
   }
 }
