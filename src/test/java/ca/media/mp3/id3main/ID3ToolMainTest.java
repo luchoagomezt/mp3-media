a3da3bbe85745574b424e6c14733ebd5e63c5a74
Luis A Gomez <luchoagomezt@gmail.com>
2018-07-13 00:16:34 -0400

    Added an renamed the test cases.

--- src/test/java/ca/media/mp3/id3main/ID3ToolMainTest.java
+++ src/test/java/ca/media/mp3/id3main/ID3ToolMainTest.java
@@ -3,7 +3,8 @@ package ca.media.mp3.id3main;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import static org.testng.Assert.assertEquals;
 
@@ -11,14 +12,19 @@ public class ID3ToolMainTest {
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final PrintStream originalOut = System.out;
   
-  @BeforeMethod
+  @BeforeClass
   public void setUpStreams() {
     System.setOut(new PrintStream(outContent));
   }
   
-  @AfterMethod
+  @AfterClass
   public void restoreStreams() {
     System.setOut(originalOut);
+  }
+  
+  @BeforeMethod
+  public void resetOutContent() {
+    outContent.reset();
   }
   
   @Test
@@ -27,4 +33,15 @@ public class ID3ToolMainTest {
     assertEquals(outContent.toString(), "");
   }
 
+  @Test
+  public void fileNameWasNotFound() {
+    ID3ToolMain.main(new String[]{"notAFile.mp3"});
+    assertEquals(outContent.toString(), String.format("%s%n", "notAFile.mp3 (The system cannot find the file specified)"));
+  }
+
+  @Test
+  public void fileNameWasFoundAndProcessed() {
+    ID3ToolMain.main(new String[]{"src/test/resources/journey.mp3"});
+    assertEquals(outContent.toString(), String.format("%s%n", "{\"size\":300022, \"revisionNumber\":0, \"flags\":0, \"majorVersion\":3}"));
+  }
 }
