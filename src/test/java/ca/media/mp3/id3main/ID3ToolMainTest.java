3d0200af0f82be26fb784c696b880c8dfb5f0f58
Luis A Gomez <luchoagomezt@gmail.com>
2018-07-25 01:30:32 -0400

    The content is actually in lower case letters

--- src/test/java/ca/media/mp3/id3main/ID3ToolMainTest.java
+++ src/test/java/ca/media/mp3/id3main/ID3ToolMainTest.java
@@ -47,7 +47,7 @@ public class ID3ToolMainTest {
   @Test
   public void fileNameWasFoundAndProcessed() {
     ID3ToolMain.main(new String[]{"src/test/resources/journey.mp3"});
-    assertEquals(outContent.toString(), String.format("%s%n", "{\"header\":{\"version\":3, \"revision\":0, \"flags\":0, \"size\":300022}, \"frames\":[{\"id\":\"TIT2\", \"size\":6, \"flags\":{\"first\":0, \"second\":0}, \"content\":\"TITLE\"}]}"));
+    assertEquals(outContent.toString(), String.format("%s%n", "{\"header\":{\"version\":3, \"revision\":0, \"flags\":0, \"size\":300022}, \"frames\":[{\"id\":\"TIT2\", \"size\":6, \"flags\":{\"first\":0, \"second\":0}, \"content\":\"Title\"}]}"));
   }
 
   @Test
