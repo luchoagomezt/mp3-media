114d6f186e5448c9c7478c35b02ff9ac8667ec91
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-02 00:10:04 -0400

    first re-factoring of the ID3Tag

--- src/main/java/ca/media/mp3/entity/ID3V2Tag.java
+++ src/main/java/ca/media/mp3/entity/ID3V2Tag.java
@@ -4,118 +4,181 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ID3V2Tag {
+public class ID3V2Tag 
+{
   public static final int HEADER_SIZE = 10;
+  private static final int MAXIMUM_REVISION_NUMBER = 255;
+  private static final int MAXIMUM_VERSION_NUMBER = 255;
+  private static final int MAXIMUM_SIZE_VALUE = 128;
   private final List<Frame> frameList;
   private final Header header;
 
-  public ID3V2Tag(final int[] mp3) {
-    if(!isAnID3V2tag(mp3)) {
-      throw new IllegalArgumentException("Array does not contain an ID3 V2 tag");
-    }
+  public ID3V2Tag(final int[] data) 
+  {
+    checkIfID3TagIsValid(data);
 
-    header = new Header(mp3);
-    frameList = buildFrames(mp3);  
+    header = new Header(data);
+    frameList = buildFrames(data);  
   }
 
-  private List<Frame> buildFrames(final int[] mp3) {
+  private void checkIfID3TagIsValid(final int[] data) 
+  {
+    if(!isAnID3V2tag(data)) {
+      throw new IllegalArgumentException("Array does not contain an ID3 V2 tag");
+    }
+  }
+
+  private List<Frame> buildFrames(final int[] data) {
     List<Frame> list = new ArrayList<Frame>();
-    if(mp3.length < Frame.HEADER_SIZE + HEADER_SIZE) {
+    if(data.length < Frame.HEADER_SIZE + HEADER_SIZE) {
       return list;
     }
     
-    int[] frameHeader = Arrays.copyOfRange(mp3, HEADER_SIZE, Frame.HEADER_SIZE + HEADER_SIZE);
+    int[] frameHeader = Arrays.copyOfRange(data, HEADER_SIZE, Frame.HEADER_SIZE + HEADER_SIZE);
     int frameSize = Frame.calculateContentSize(frameHeader);
-    int[] frameArray = Arrays.copyOfRange(mp3, HEADER_SIZE, Frame.HEADER_SIZE + HEADER_SIZE + frameSize);
+    int[] frameArray = Arrays.copyOfRange(data, HEADER_SIZE, Frame.HEADER_SIZE + HEADER_SIZE + frameSize);
     list.add(new Frame(frameArray));
     return list;
   }
 
-  public static int calculateTagSizeExcludingHeader(final int[] header) {
-    if (header == null) {
-      throw new IllegalArgumentException("Parameter is null");
-    }
+  public static int calculateTagSizeExcludingHeader(final int[] header) 
+  {
+    checkIfHeaderIsNull(header);
+    checkIfHeaderIsTooShort(header);
+    checkIfSizeDescriptorIsValid(header); 
 
+    return (int)(
+      header[6] * Math.pow(MAXIMUM_SIZE_VALUE, 3) + 
+      header[7] * Math.pow(MAXIMUM_SIZE_VALUE, 2) +
+      header[8] * MAXIMUM_SIZE_VALUE +
+      header[9]);
+  }
+
+  private static void checkIfSizeDescriptorIsValid(final int[] header) 
+  {
+    if(header[6] > 0x7F || header[7] > 0x7F || header[8] > 0x7F || header[9] > 0x7F) {
+      throw new IllegalArgumentException("One or more of the four size bytes is more than 0x7F");
+    }
+  }
+
+  private static void checkIfHeaderIsTooShort(final int[] header) 
+  {
     if (header.length < HEADER_SIZE) {
       throw new IllegalArgumentException("Array's length is less than header's size");
     }
-    
-    if(header[6] > 0x7F || header[7] > 0x7F || header[8] > 0x7F || header[9] > 0x7F) {
-      throw new IllegalArgumentException("One or more of the four size bytes is more than 0x7F");
-    } 
-
-    return ((header[6] * 0x80 +header[7]) * 0x80 + header[8]) * 0x80 + header[9];
   }
 
-  public static boolean isAnID3V2tag(final int[] mp3) {
-    if(mp3 == null) {
+  private static void checkIfHeaderIsNull(final int[] header) 
+  {
+    if (header == null) {
+      throw new IllegalArgumentException("Parameter is null");
+    }
+  }
+
+  public static boolean isAnID3V2tag(final int[] data) 
+  {
+    if(data == null) {
       throw new IllegalArgumentException("Parameter array is null");
     }
 
-    if(mp3.length < HEADER_SIZE) {
-      throw new IllegalArgumentException("Array's length is less than header's size");
-    }
+    checkIfHeaderIsTooShort(data);
 
     return 
-        mp3[0] == 0x49 &&
-        mp3[1] == 0x44 &&
-        mp3[2] == 0x33 &&
-        mp3[3] < 0xFF &&
-        mp3[4] < 0xFF &&
-        mp3[5] == (mp3[5] & 0xE0) &&
-        mp3[6] < 0x80 &&
-        mp3[7] < 0x80 &&
-        mp3[8] < 0x80 &&
-        mp3[9] < 0x80;
+        data[0] == 0x49 &&
+        data[1] == 0x44 &&
+        data[2] == 0x33 &&
+        data[3] < MAXIMUM_VERSION_NUMBER &&
+        data[4] < MAXIMUM_REVISION_NUMBER &&
+        data[5] == (data[5] & 0xE0) &&
+        data[6] < MAXIMUM_SIZE_VALUE &&
+        data[7] < MAXIMUM_SIZE_VALUE &&
+        data[8] < MAXIMUM_SIZE_VALUE &&
+        data[9] < MAXIMUM_SIZE_VALUE;
   }
     
   public int majorVersion() {
-    return header.majorVersion;
+    return getHeader().getMajorVersion();
   }
   
   public int revisionNumber() {
-    return header.revisionNumber;
+    return getHeader().getRevisionNumber();
   }
   
   public boolean unsynchronisation() {
-    return (header.flag & 0x80) == 0x80;
+    return (getHeader().getFlag() & 0x80) == 0x80;
   }
   
   public boolean extendedHeader() {
-    return (header.flag & 0x40) == 0x40;
+    return (getHeader().getFlag() & 0x40) == 0x40;
   }
   
   public boolean experimental() {
-    return (header.flag & 0x20) == 0x20;
+    return (getHeader().getFlag() & 0x20) == 0x20;
   }
   
   public int flags() {
-    return header.flag;
+    return getHeader().getFlag();
   }
   
   public int size() {
-    return header.size;
+    return getHeader().getSize();
   }
   
   @Override
   public String toString() {
     return 
-      String.format("{\"header\":{\"version\":%d, \"revision\":%d, \"flags\":%d, \"size\":%d}, \"frames\":[%s]}", 
-      majorVersion(), revisionNumber(), flags(), size(), frameList.stream().map(e -> e.toString()).
-      reduce((s1, s2) -> s1.concat(", ").concat(s2)).orElse(""));
+      String.format("%s, \"frames\":[%s]}", 
+      getHeader().toString(), 
+      getFrameList().
+      stream().
+      map(e -> e.toString()).
+      reduce((s1, s2) -> s1.concat(", ").
+      concat(s2)).
+      orElse(""));
   }
   
-  private class Header {
+  private Header getHeader() {
+    return header;
+  }
+
+  private List<Frame> getFrameList() {
+    return frameList;
+  }
+
+  private class Header 
+  {
     private final int majorVersion;
     private final int revisionNumber;
     private final int flag;
     private final int size;
     
-    Header(final int[] data) {
+    public Header(final int[] data) {
       majorVersion = data[3];
       revisionNumber = data[4];
       flag = data[5];
       size = calculateTagSizeExcludingHeader(data);      
+    }
+
+    public int getMajorVersion() {
+      return majorVersion;
+    }
+
+    public int getRevisionNumber() {
+      return revisionNumber;
+    }
+
+    public int getFlag() {
+      return flag;
+    }
+
+    public int getSize() {
+      return size;
+    }
+    
+    public String toString() {
+      return 
+        String.format("{\"header\":{\"version\":%d, \"revision\":%d, \"flags\":%d, \"size\":%d}", 
+        majorVersion(), revisionNumber(), flags(), size());
     }
   }
 }
