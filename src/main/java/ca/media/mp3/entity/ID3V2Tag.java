d874419e29cbad8c1d9484cc95da0b64638c4fc5
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-04 16:29:19 -0400

    Moving all header methods

--- src/main/java/ca/media/mp3/entity/ID3V2Tag.java
+++ src/main/java/ca/media/mp3/entity/ID3V2Tag.java
@@ -7,13 +7,6 @@ import java.util.List;
 public class ID3V2Tag 
 {
   public static final int HEADER_SIZE = 10;
-  private static final int CHAR_I = 0x49;
-  private static final int CHAR_D = 0x44;
-  private static final int CHAR_3 = 0x33;
-  private static final int MAXIMUM_REVISION_NUMBER = 0xFF;
-  private static final int MAXIMUM_VERSION_NUMBER = 0xFF;
-  private static final int FLAG_MASK = 0xE0;
-  private static final int MAXIMUM_SIZE_VALUE = 0x80;
   private final List<Frame> frameList;
   private final Header header;
 
@@ -25,79 +18,8 @@ public class ID3V2Tag
     frameList = buildFrameList(data);  
   }
 
-  public List<Frame> getFrameList() {
-    return frameList;
-  }
-
-  public Header getHeader() {
-    return header;
-  }
-
-  public static int calculateTagSize(final int[] data) 
+  private List<Frame> buildFrameList(final int[] data)
   {
-    checkIfDataIsNull(data);
-    checkIfDataIsTooShort(data);
-    checkIfSizeDescriptorIsValid(data); 
-
-    return calculateSize(data);
-  }
-
-  public static boolean isAnID3V2tag(final int[] data) 
-  {
-    checkIfDataIsNull(data);
-    checkIfDataIsTooShort(data);
-
-    return isATagPattern(data);
-  }
-
-  public int majorVersion() {
-    return getHeader().getMajorVersion();
-  }
-  
-  public int revisionNumber() {
-    return getHeader().getRevisionNumber();
-  }
-
-  public boolean unsynchronisation() {
-    return (getHeader().getFlag() & 0x80) == 0x80;
-  }
-  
-  public boolean extendedHeader() {
-    return (getHeader().getFlag() & 0x40) == 0x40;
-  }
-  
-  public boolean experimental() {
-    return (getHeader().getFlag() & 0x20) == 0x20;
-  }
-  
-  public int flags() {
-    return getHeader().getFlag();
-  }
-  
-  public int size() {
-    return getHeader().getSize();
-  }
-
-  @Override
-  public String toString() {
-    return 
-      String.format("%s, \"frames\":[%s]}", 
-      getHeader().toString(), 
-      getFrameList().
-      stream().
-      map(e -> e.toString()).
-      reduce((s1, s2) -> s1.concat(", ").concat(s2)).
-      orElse(""));
-  }
-
-  private void checkIfID3TagIsValid(final int[] data) 
-  {
-    if(!isAnID3V2tag(data)) {
-      throw new IllegalArgumentException("Array does not contain an ID3 V2 tag");
-    }
-  }
-
-  private List<Frame> buildFrameList(final int[] data) {
     List<Frame> list = new ArrayList<Frame>();
     if(data.length < Frame.HEADER_SIZE + HEADER_SIZE) {
       return list;
@@ -110,18 +32,33 @@ public class ID3V2Tag
     return list;
   }
 
-  private static int calculateSize(final int[] data) {
-    return (int)(
-      data[6] * Math.pow(MAXIMUM_SIZE_VALUE, 3) + 
-      data[7] * Math.pow(MAXIMUM_SIZE_VALUE, 2) +
-      data[8] * MAXIMUM_SIZE_VALUE +
-      data[9]);
-    }
-
-  private static void checkIfSizeDescriptorIsValid(final int[] data) 
+  public static int calculateTagSize(final int[] data) 
   {
-    if(data[6] >= MAXIMUM_SIZE_VALUE || data[7] >= MAXIMUM_SIZE_VALUE || 
-      data[8] >= MAXIMUM_SIZE_VALUE || data[9] >= MAXIMUM_SIZE_VALUE) 
+    checkIfDataIsNull(data);
+    checkIfDataIsTooShort(data);
+    checkIfSizeDescriptorsAreValid(data); 
+
+    return Header.calculateSize(data);
+  }
+
+  public static boolean isAnID3V2tag(final int[] data) 
+  {
+    checkIfDataIsNull(data);
+    checkIfDataIsTooShort(data);
+
+    return Header.isATagPattern(data);
+  }
+
+  private void checkIfID3TagIsValid(final int[] data) 
+  {
+    if(!isAnID3V2tag(data)) {
+      throw new IllegalArgumentException("Array does not contain an ID3 V2 tag");
+    }
+  }
+
+  private static void checkIfSizeDescriptorsAreValid(final int[] data) 
+  {
+    if(!Header.areSizeDescriptorsValid(data)) 
     {
       throw new IllegalArgumentException("One or more of the four size bytes is more than 0x7F");
     }
@@ -141,23 +78,61 @@ public class ID3V2Tag
     }
   }
 
-  private static boolean isATagPattern(final int[] data)
+  public int majorVersion()
   {
-    return 
-      data[0] == CHAR_I &&
-      data[1] == CHAR_D &&
-      data[2] == CHAR_3 &&
-      data[3] < MAXIMUM_VERSION_NUMBER &&
-      data[4] < MAXIMUM_REVISION_NUMBER &&
-      data[5] == (data[5] & FLAG_MASK) &&
-      data[6] < MAXIMUM_SIZE_VALUE &&
-      data[7] < MAXIMUM_SIZE_VALUE &&
-      data[8] < MAXIMUM_SIZE_VALUE &&
-      data[9] < MAXIMUM_SIZE_VALUE;
+    return header.getMajorVersion();
+  }
+  
+  public int revisionNumber()
+  {
+    return header.getRevisionNumber();
   }
 
-  private class Header 
+  public boolean unsynchronisation()
   {
+    return header.isUnsynchronized();
+  }
+
+  public boolean extendedHeader()
+  {
+    return header.hasAnExtendedHeader();
+  }
+
+  public boolean experimental()
+  {
+    return header.isExperimental();
+  }
+
+  public int size()
+  {
+    return header.getSize();
+  }
+
+  @Override
+  public String toString()
+  {
+    return 
+      String.format("%s, \"frames\":[%s]}", 
+      header.toString(), 
+      frameList.
+      stream().
+      map(e -> e.toString()).
+      reduce((s1, s2) -> s1.concat(", ").concat(s2)).
+      orElse(""));
+  }
+
+  private static class Header 
+  {
+    private static final int CHAR_I = 0x49;
+    private static final int CHAR_D = 0x44;
+    private static final int CHAR_3 = 0x33;
+    private static final int MAXIMUM_REVISION_NUMBER = 0xFF;
+    private static final int MAXIMUM_VERSION_NUMBER = 0xFF;
+    private static final int EXPERIMENTAL_BIT = 0x20;
+    private static final int EXTENDED_BIT = 0x40;
+    private static final int UNSYNCHRONISATION_BIT = 0x80;
+    private static final int FLAG_MASK = 0xE0;
+    private static final int MAXIMUM_SIZE_VALUE = 0x80;
     private final int majorVersion;
     private final int revisionNumber;
     private final int flag;
@@ -171,27 +146,75 @@ public class ID3V2Tag
       size = calculateTagSize(data);      
     }
 
-    public int getMajorVersion() {
+    private static boolean isATagPattern(final int[] data)
+    {
+      return 
+        data[0] == CHAR_I &&
+        data[1] == CHAR_D &&
+        data[2] == CHAR_3 &&
+        data[3] < MAXIMUM_VERSION_NUMBER &&
+        data[4] < MAXIMUM_REVISION_NUMBER &&
+        data[5] == (data[5] & FLAG_MASK) &&
+        data[6] < MAXIMUM_SIZE_VALUE &&
+        data[7] < MAXIMUM_SIZE_VALUE &&
+        data[8] < MAXIMUM_SIZE_VALUE &&
+        data[9] < MAXIMUM_SIZE_VALUE;
+    }
+
+    public static int calculateSize(final int[] data)
+    {
+      return (int)(
+        data[6] * Math.pow(MAXIMUM_SIZE_VALUE, 3) + 
+        data[7] * Math.pow(MAXIMUM_SIZE_VALUE, 2) +
+        data[8] * MAXIMUM_SIZE_VALUE +
+        data[9]);
+      }
+
+    public static boolean areSizeDescriptorsValid(int[] data)
+    {
+      return 
+        data[6] < MAXIMUM_SIZE_VALUE && 
+        data[7] < MAXIMUM_SIZE_VALUE && 
+        data[8] < MAXIMUM_SIZE_VALUE &&
+        data[9] < MAXIMUM_SIZE_VALUE;
+    }
+
+    public int getMajorVersion()
+    {
       return majorVersion;
     }
 
-    public int getRevisionNumber() {
+    public int getRevisionNumber()
+    {
       return revisionNumber;
     }
 
-    public int getFlag() {
-      return flag;
-    }
-
-    public int getSize() {
-      return size;
+    public boolean isUnsynchronized()
+    {
+      return (flag & UNSYNCHRONISATION_BIT) == UNSYNCHRONISATION_BIT;
     }
     
-    public String toString() {
+    public boolean hasAnExtendedHeader()
+    {
+      return (flag & EXTENDED_BIT) == EXTENDED_BIT;
+    }
+
+    public boolean isExperimental()
+    {
+      return (flag & EXPERIMENTAL_BIT) == EXPERIMENTAL_BIT;
+    }
+
+    public int getSize()
+    {
+      return size;
+    }
+
+    public String toString()
+    {
       return 
         String.format(
           "{\"header\":{\"version\":%d, \"revision\":%d, \"flags\":%d, \"size\":%d}",
-          majorVersion(), revisionNumber(), flags(), size());
+          majorVersion, revisionNumber, flag, size);
     }
   }
 }
