f05f7cf085823c7ee5dbc05e01d72e0ab1ff88d6
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-07 01:04:28 -0400

    Removing null check, it is unnecessary.

--- src/main/java/ca/media/mp3/adapter/SimpleFormatter.java
+++ src/main/java/ca/media/mp3/adapter/SimpleFormatter.java
@@ -3,14 +3,11 @@ package ca.media.mp3.adapter;
 import ca.media.mp3.application.ID3TagFormatter;
 import ca.media.mp3.entity.ID3V2Tag;
 
-public class SimplePresenter implements ID3TagFormatter {
+public class SimpleFormatter implements ID3TagFormatter {
 
   @Override
-  public String tagToString(ID3V2Tag tag) {
-    if(tag != null) {
-      return tag.toString();
-    }
-    return null;
+  public String format(ID3V2Tag tag) {
+    return tag.toString();
   }
 
 }
