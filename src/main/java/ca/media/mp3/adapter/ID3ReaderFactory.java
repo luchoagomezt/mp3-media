fa15e0545b961011fbc0822c6a00865c99e4c05a
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-07 01:13:26 -0400

    Moving exception handling from the main method to a factory

--- src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
+++ src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
@@ -1,16 +1,27 @@
 package ca.media.mp3.adapter;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import ca.media.mp3.application.ID3Reader;
 import ca.media.mp3.application.ID3Tool;
+import ca.media.mp3.application.MP3MediaException;
 
 public class ID3ReaderFactory {
-  public static final String SIMPLE_PRESENTER = "SIMPLE_PRESENTER";
 
-  public static ID3Reader makeAnID3Reader(String presenterName) {
-    if (presenterName.equals(SIMPLE_PRESENTER)) {
-      return new ID3Tool(new SimplePresenter());
+  public static ID3Reader makeAnID3Reader(String filePath) {
+    try {
+      final InputStream mp3File = new BufferedInputStream(new FileInputStream(filePath));
+      ID3Reader reader = new ID3Tool(mp3File);
+      return reader;
+    } catch (FileNotFoundException e) {
+      throw new MP3MediaException(filePath + " (No such file or directory)");
+    } catch (IllegalArgumentException e) {
+      throw new MP3MediaException(e);
     }
-    return null;
+
   }
 
 }
--- src/main/java/ca/media/mp3/id3main/ID3ToolMain.java
+++ src/main/java/ca/media/mp3/id3main/ID3ToolMain.java
@@ -1,31 +1,24 @@
 package ca.media.mp3.id3main;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
 import ca.media.mp3.adapter.ID3ReaderFactory;
-import static ca.media.mp3.adapter.ID3ReaderFactory.SIMPLE_PRESENTER;
+import ca.media.mp3.adapter.SimpleFormatter;
 import ca.media.mp3.application.ID3Reader;
+import ca.media.mp3.application.ID3TagFormatter;
+import ca.media.mp3.application.MP3MediaException;
 
 public class ID3ToolMain {
   
   public static void main(String[] args) {
     if(args.length == 0) {
+      System.out.printf("%s%n", "usage: id3 <path to MP3 file>");
       return;
     }
     
-    try (final InputStream mp3File = new BufferedInputStream(new FileInputStream(args[0]))) {
-      ID3Reader tool = ID3ReaderFactory.makeAnID3Reader(SIMPLE_PRESENTER);
-      tool.read(mp3File);
-      System.out.println(tool.perform());
-    } catch (FileNotFoundException e) {
-      System.out.println(args[0] + " (No such file or directory)");
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    } catch (IllegalArgumentException e) {
+    try {
+      ID3Reader tool = ID3ReaderFactory.makeAnID3Reader(args[0]);
+      ID3TagFormatter formatter = new SimpleFormatter();
+      System.out.println(formatter.format(tool.perform()));
+    } catch (MP3MediaException e) {
       System.out.println(e.getMessage());
     }
   }
