7557d99a99c30908d47e83d3dd836668c9d68b40
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-16 23:45:14 -0400

    forgot to the factory

--- src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
+++ src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
@@ -1,10 +1,11 @@
-package ca.media.mp3.application;
+package ca.media.mp3.adapter;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import ca.media.mp3.application.ID3Tool;
 import ca.media.mp3.entity.ID3Reader;
 import ca.media.mp3.entity.MP3MediaException;
 
