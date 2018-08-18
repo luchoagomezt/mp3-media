63ddb20d702e2a9a6ecdaf58dbb9add6ba9e933a
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-18 15:34:18 -0400

    Adding a new module for the web.

---
+++ id3web/src/main/java/ca/media/mp3/web/App.java
@@ -0,0 +1,13 @@
+package ca.media.mp3.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
