14ccfc7a6b00788184908fa1b33285ce53c0f8d1
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-18 15:28:18 -0400

    Removing log4j2

--- id3console/src/main/java/ca/media/mp3/id3main/ID3ToolMain.java
+++ id3console/src/main/java/ca/media/mp3/id3main/ID3ToolMain.java
@@ -7,27 +7,19 @@ import ca.media.mp3.adapter.Presenter;
 import ca.media.mp3.adapter.SimpleFormatter;
 import ca.media.mp3.entity.ID3Reader;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class ID3ToolMain {
-  private static final Logger logger = LogManager.getLogger("ID3ToolMain.class");
   
   public static void main(String[] args) {
-    logger.debug("starting tool");
 
     Presenter presenter = new ConsolePresenter();
     if(args.length == 0) {
-      logger.debug("args array is empty, stopping tool");
       presenter.display("usage: id3 <path to MP3 file>");
       return;
     }
     
-    logger.debug("args[0]: " + args[0]);
     ID3Reader tool = ID3ReaderFactory.makeAnID3Reader(args[0]);
     ID3TagFormatter formatter = new SimpleFormatter();
     presenter.display((formatter.format(tool.perform())));
 
-    logger.debug("stopping tool");
   }
 }
