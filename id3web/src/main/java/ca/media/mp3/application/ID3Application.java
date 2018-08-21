a8eff13a93733d08d2ab3d1ddad507ab60de5121
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-21 16:54:32 -0400

    Adding the all the required packages to the base packages

--- id3web/src/main/java/ca/media/mp3/application/ID3Application.java
+++ id3web/src/main/java/ca/media/mp3/application/ID3Application.java
@@ -3,8 +3,11 @@ package ca.media.mp3.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ca.media.mp3.controller")
-public class ID3Application 
+@SpringBootApplication(scanBasePackages = {
+  "ca.media.mp3.entity",
+  "ca.media.mp3.controller",
+  "ca.media.mp3.service"})
+public class ID3Application
 {
   public static void main(String[] args)
   {
