47667145a0b18b4523bbfe99832ecd81f60f9e5d
Luis A Gomez <luchoagomezt@gmail.com>
2018-08-21 16:51:26 -0400

    Deleting the configuration class and renaming the reader tool

--- id3web/src/main/java/ca/media/mp3/service/ID3ServiceProperties.java
+++
@@ -1,9 +0,0 @@
-package ca.media.mp3.service;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("service")
-public class ID3ServiceProperties 
-{
-
-}
