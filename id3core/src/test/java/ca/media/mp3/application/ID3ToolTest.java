a13d711e5898ee1af71968957827fcc936c3b942
LUIS ALFONSO GOMEZ <LUIS.GOMEZ@cbc.ca>
2018-08-22 17:08:32 -0400

    re-factoring the test

--- id3core/src/test/java/ca/media/mp3/application/ID3ToolTest.java
+++ id3core/src/test/java/ca/media/mp3/application/ID3ToolTest.java
@@ -16,14 +16,15 @@ public class ID3ToolTest {
   
   @Test
   public void performOnMP3WithAnID3V2TagWithoutFrames() throws IOException {
-    ID3Tool tool = new ID3Tool(new ByteArrayInputStream(new byte[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x00, 0x00}));
+    byte[] tagWithoutFrames = new byte[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x00, 0x00};
+	ID3Tool tool = makeID3ToolFrom(tagWithoutFrames);
     assertEquals(tool.perform().toString(), "{\"version\":3, \"revision\":0, \"flags\":0, \"size\":0, \"frames\":[]}");
   }
 
   @Test
   public void performOnMP3WithAnID3V2TagWithOneFrame() throws IOException {
-    byte[] data = new byte[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x00, 0x0A, 0x49, 0x49, 0x49, 0x49, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    ID3Tool tool = new ID3Tool(new ByteArrayInputStream(data));
+    byte[] tagWithOneFrame = new byte[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x00, 0x0A, 0x49, 0x49, 0x49, 0x49, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    ID3Tool tool = makeID3ToolFrom(tagWithOneFrame);
     String expected = "{\"version\":3, \"revision\":0, \"flags\":0, \"size\":10, \"frames\":[]}";
     assertEquals(tool.perform().toString(), expected);
   }
@@ -39,6 +40,10 @@ public class ID3ToolTest {
     InputStream stream = new ByteArrayInputStream(new byte[]{60, 60, 33, 03, 00, 00});
     ID3Tool tool = new ID3Tool(stream);
     tool.perform();
+  }
+
+  private ID3Tool makeID3ToolFrom(byte[] tagWithoutFrames) {
+	  return new ID3Tool(new ByteArrayInputStream(tagWithoutFrames));
   }
 
 }
