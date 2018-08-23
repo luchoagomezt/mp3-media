e94eaa91ba8072856019b19ac046b67cad3f323b
LUIS ALFONSO GOMEZ <LUIS.GOMEZ@cbc.ca>
2018-08-23 09:55:43 -0400

    This interface does not need a dependency

--- id3web/src/main/java/ca/media/mp3/service/ID3WebReader.java
+++ id3web/src/main/java/ca/media/mp3/service/ID3WebReader.java
@@ -1,8 +1,9 @@
 package ca.media.mp3.service;
 
-import ca.media.mp3.entity.ID3Reader;
+import ca.media.mp3.entity.ID3V2Tag;
 
-public interface ID3WebReader extends ID3Reader 
+public interface ID3WebReader 
 {
+  ID3V2Tag perform();
   void setUrl(String url);
 }
