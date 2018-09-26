207970299aba6205273d949cf7ab2da26a5b92d5
Luis A Gomez <luchoagomezt@gmail.com>
2018-09-25 20:33:17 -0400

    fixing a test case failure

--- id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
+++ id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
@@ -3,7 +3,6 @@ package ca.media.mp3.adapter;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 
 import ca.media.mp3.entity.ID3Reader;
@@ -16,17 +15,10 @@ public class ID3ReaderFactory {
 
   public static ID3Reader makeAnID3Reader(String filePath) {
     try {
-      InputStream inputStream = new FileInputStream(filePath);
-      try {
-        final InputStream mp3File = new BufferedInputStream(inputStream);
-        return new ID3Tool(mp3File);
-      } finally {
-        inputStream.close();
-      }
+      final InputStream mp3File = new BufferedInputStream(new FileInputStream(filePath));
+      return new ID3Tool(mp3File);
     } catch (FileNotFoundException e) {
       throw new MP3MediaException(filePath + " (No such file or directory)");
-    } catch (IOException e) {
-      throw new MP3MediaException("Cannot close the input stream");
     }
 
   }
