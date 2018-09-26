212c8b437dbf789fd2ec4869917cc09337273aa1
Luis A Gomez <luchoagomezt@gmail.com>
2018-09-25 20:21:10 -0400

    Removing smell

--- id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
+++ id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
@@ -3,6 +3,7 @@ package ca.media.mp3.adapter;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 import ca.media.mp3.entity.ID3Reader;
@@ -11,16 +12,21 @@ import ca.media.mp3.usercase.ID3Tool;
 
 public class ID3ReaderFactory {
   private ID3ReaderFactory() {
-    throw new IllegalStateException();
   }
 
   public static ID3Reader makeAnID3Reader(String filePath) {
     try {
-      final InputStream mp3File = new BufferedInputStream(new FileInputStream(filePath));
-      ID3Reader reader = new ID3Tool(mp3File);
-      return reader;
+      InputStream inputStream = new FileInputStream(filePath);
+      try {
+        final InputStream mp3File = new BufferedInputStream(inputStream);
+        return new ID3Tool(mp3File);
+      } finally {
+        inputStream.close();
+      }
     } catch (FileNotFoundException e) {
       throw new MP3MediaException(filePath + " (No such file or directory)");
+    } catch (IOException e) {
+      throw new MP3MediaException("Cannot close the input stream");
     }
 
   }
--- id3console/src/test/java/ca/media/mp3/adapter/ID3ReaderFactoryTest.java
+++ id3console/src/test/java/ca/media/mp3/adapter/ID3ReaderFactoryTest.java
@@ -6,7 +6,16 @@ import ca.media.mp3.entity.MP3MediaException;
 
 import static org.testng.Assert.assertNotNull;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class ID3ReaderFactoryTest {
+
+  @Test(expectedExceptions = {IllegalAccessException.class})
+  public void instantiateAFactory() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IllegalArgumentException {
+    Constructor<ID3ReaderFactory> constructor = ID3ReaderFactory.class.getDeclaredConstructor();
+    constructor.newInstance();
+  }
 
   @Test(expectedExceptions = {MP3MediaException.class})
   public void checkFileNotFound() {
