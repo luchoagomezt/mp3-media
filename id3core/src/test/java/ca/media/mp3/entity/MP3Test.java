6a9b62aab0c8840a2f56d60a9f9d46ea2075568b
Luis A Gomez <luchoagomezt@gmail.com>
2018-09-26 22:03:03 -0400

    Removing code smells

--- id3core/src/test/java/ca/media/mp3/entity/MP3Test.java
+++ id3core/src/test/java/ca/media/mp3/entity/MP3Test.java
@@ -9,18 +9,15 @@ import static org.testng.Assert.assertEquals;
 
 public class MP3Test
 {
-  final static ID3V2Tag TAG = new ID3V2Tag(new int[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x02, 0x00});
+  static final ID3V2Tag TAG = new ID3V2Tag(new int[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x02, 0x00});
 
   @Test
-  public void constructAMP3AndGetTheArguments() 
+  public void constructAMP3AndGetTheArguments() throws MalformedURLException
   {
-    try {
-      URL url = new URL("file", "localhost", "/home/luis");
-      MP3 mp3 = new MP3(url, TAG);
-      assertEquals(mp3.getTag(), TAG);
-      assertEquals(mp3.getURL(), url);
-    } catch(MalformedURLException e) {  
-    }
+    URL url = new URL("file", "localhost", "/home/luis");
+    MP3 mp3 = new MP3(url, TAG);
+    assertEquals(mp3.getTag(), TAG);
+    assertEquals(mp3.getURL(), url);
   }
   
   @Test
