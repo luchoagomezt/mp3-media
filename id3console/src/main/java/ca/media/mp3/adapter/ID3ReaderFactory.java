7861182b21bddb97bb21f7620c068741edd505fc
Luis A Gomez <luchoagomezt@gmail.com>
2018-09-26 20:36:44 -0400

    Adding another public constructor to the tool.

--- id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
+++ id3console/src/main/java/ca/media/mp3/adapter/ID3ReaderFactory.java
@@ -1,12 +1,6 @@
 package ca.media.mp3.adapter;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 import ca.media.mp3.entity.ID3Reader;
-import ca.media.mp3.entity.MP3MediaException;
 import ca.media.mp3.usercase.ID3Tool;
 
 public class ID3ReaderFactory {
@@ -14,13 +8,7 @@ public class ID3ReaderFactory {
   }
 
   public static ID3Reader makeAnID3Reader(String filePath) {
-    try {
-      final InputStream mp3File = new BufferedInputStream(new FileInputStream(filePath));
-      return new ID3Tool(mp3File);
-    } catch (FileNotFoundException e) {
-      throw new MP3MediaException(filePath + " (No such file or directory)");
-    }
-
+    return new ID3Tool(filePath);
   }
 
 }
--- id3core/src/test/java/ca/media/mp3/application/ID3ToolTest.java
+++ id3core/src/test/java/ca/media/mp3/application/ID3ToolTest.java
@@ -16,7 +16,7 @@ public class ID3ToolTest {
   @Test
   public void performOnMP3WithAnID3V2TagWithoutFrames() throws IOException {
     byte[] tagWithoutFrames = new byte[]{0x49, 0x44, 0x33, 0x03, 00, 00, 00, 00, 0x00, 0x00};
-	ID3Tool tool = makeID3ToolFrom(tagWithoutFrames);
+    ID3Tool tool = makeID3ToolFrom(tagWithoutFrames);
     assertEquals(tool.perform().toString(), "{\"version\":3, \"revision\":0, \"flags\":0, \"size\":0, \"frames\":[]}");
   }
 
@@ -31,7 +31,7 @@ public class ID3ToolTest {
   @Test(expectedExceptions = {MP3MediaException.class}, expectedExceptionsMessageRegExp = "The stream does not contain an ID3 V2 tag")
   public void performOnMP3WithoutID3V2Tag() throws IOException {
     byte[] tagWithoutFrames = new byte[]{60, 60, 33, 03, 00, 00, 00, 00, 0x02, 0x00};
-	ID3Tool tool = makeID3ToolFrom(tagWithoutFrames);
+    ID3Tool tool = makeID3ToolFrom(tagWithoutFrames);
     tool.perform();
   }
 
@@ -42,8 +42,13 @@ public class ID3ToolTest {
     tool.perform();
   }
 
+  @Test(expectedExceptions = {MP3MediaException.class})
+  public void throwsExceptionWhenfileDoesNotExit() {
+    new ID3Tool("/home/does/not/exist/file.txt");
+  }
+
   private ID3Tool makeID3ToolFrom(byte[] tag) {
-	  return new ID3Tool(new ByteArrayInputStream(tag));
+    return new ID3Tool(new ByteArrayInputStream(tag));
   }
 
 }
