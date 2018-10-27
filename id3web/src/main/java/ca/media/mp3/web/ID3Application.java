b09476d252403ae2d44b551741bfb3972d640b6f
LUIS ALFONSO GOMEZ <LUIS.GOMEZ@cbc.ca>
2018-10-27 16:49:22 -0400

    Adding the service and controller to the scan base

--- id3web/src/main/java/ca/media/mp3/web/ID3Application.java
+++ id3web/src/main/java/ca/media/mp3/web/ID3Application.java
@@ -3,7 +3,9 @@ package ca.media.mp3.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+"ca.media.mp3.web.controller",
+"ca.media.mp3.web.service"})
 public class ID3Application 
 {
   public static void main(String[] args)
