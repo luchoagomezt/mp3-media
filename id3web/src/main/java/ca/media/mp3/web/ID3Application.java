0755e6f475a9dc10f58ce4dbec72e830100bc4e0
LUIS ALFONSO GOMEZ <LUIS.GOMEZ@cbc.ca>
2018-10-27 15:44:45 -0400

    Adding hello spring boot

---
+++ id3web/src/main/java/ca/media/mp3/web/ID3Application.java
@@ -0,0 +1,13 @@
+package ca.media.mp3.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ID3Application 
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(ID3Application.class, args);  
+  }
+}
---
+++ id3web/src/test/java/ca/media/mp3/web/ID3ApplicationTest.java
@@ -0,0 +1,16 @@
+package ca.media.mp3.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ID3ApplicationTest 
+{
+  @Test
+  public void contextLoads() 
+  {	  
+  }
+}
